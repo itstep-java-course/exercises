f53efc3ee1344999c0e6fcf8d8b1d0cef165d4e7
Strahov2023 <obormots140@gmail.com>
2019-10-23 19:57:00 +0300

    comment fo java

--- task1/src/main/java/com/itstep/task1/Test.java
+++ task1/src/main/java/com/itstep/task1/Test.java
@@ -3,5 +3,6 @@ package com.itstep.task1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Java!!!");
+        System.out.println("Hello its me!!!");
     }
 }
