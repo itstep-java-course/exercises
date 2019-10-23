0c8eca2e07dea2e562bfa4e02eb9a34680c07d3a
VladimirDV2019 <tezzrere@gmail.cm>
2019-10-23 19:56:50 +0300

    Just for test

--- task1/src/main/java/com/itstep/task1/Test.java
+++ task1/src/main/java/com/itstep/task1/Test.java
@@ -1,7 +1,11 @@
 package com.itstep.task1;
 
-public class Test {
-    public static void main(String[] args) {
+public class Test
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello Java!!!");
+        int b=5;
+        System.out.println(b);
     }
 }
