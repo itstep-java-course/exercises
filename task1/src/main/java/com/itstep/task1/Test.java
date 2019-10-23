1d18d4e6d6ae86f642e8484d3ea9e5f19d52e467
Artem_87 <tema_lihoy@mail.ru>
2019-10-23 19:55:42 +0300

    Comment

--- task1/src/main/java/com/itstep/task1/Test.java
+++ task1/src/main/java/com/itstep/task1/Test.java
@@ -1,7 +1,10 @@
-package com.itstep.task1;
+package main.java.com.itstep.task1;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Hello Java!!!");
+
+        System.out.println("Hello again!!!");
     }
 }
