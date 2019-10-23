8c02231bf58a5545009d372baca54b75f42e210d
Olezhka <oleh.chubuk@nure.ua>
2019-10-23 19:57:17 +0300

    I have some tasks, i do thomething.

--- task1/src/main/java/com/itstep/task1/Test.java
+++ task1/src/main/java/com/itstep/task1/Test.java
@@ -1,7 +1,11 @@
 package com.itstep.task1;
 
 public class Test {
+    int a;
+    a=15;
+    a+=150;
     public static void main(String[] args) {
         System.out.println("Hello Java!!!");
+        System.out.println(a);
     }
 }
