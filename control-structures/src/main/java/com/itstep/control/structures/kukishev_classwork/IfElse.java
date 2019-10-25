812869f49391993cd84f1f0bc23d1e1991397ea5
GabrielGrehov <gabriel1grehov@gmail.com>
2019-10-25 20:20:51 +0300

    If Else

---
+++ control-structures/src/main/java/com/itstep/control/structures/kukishev_classwork/IfElse.java
@@ -0,0 +1,23 @@
+package com.itstep.control.structures.kukishev_classwork;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int x = 655;
+
+        if (x == 6) {
+            System.out.println("6");
+        } else if (x == 3) {
+            System.out.println("3");
+        } else if (x <= 5) {
+            System.out.println("5");
+        } else {
+            System.out.println("0");
+        }
+
+
+    }
+
+
+}
