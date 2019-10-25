9b8d71d3ab36d02918f127a26cfe3d6b1a0795d9
Aleksandr Huk <Gook.goro@gmail.com>
2019-10-25 20:21:10 +0300

    If Else Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/huk_classwork/IfElseTask.java
@@ -0,0 +1,22 @@
+package com.itstep.control.structures.huk_classwork;
+
+public class IfElseTask {
+    public static void main(String[] args) {
+        int x = 7;
+
+        if (x == 6) {
+            System.out.println("x = 6");
+        } else if (x == 3) {
+            System.out.println("x = 3");
+        } else if (x <= 5) {
+            System.out.println("x = 5");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
+
+
+
+
+
