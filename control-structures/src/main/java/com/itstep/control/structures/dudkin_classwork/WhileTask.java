25670b0dc2a6c6f78c16f89b9ad56447ed8eb13f
VladimirDV2019 <tezzrere@gmail.com>
2019-10-25 21:12:03 +0300

    While

---
+++ control-structures/src/main/java/com/itstep/control/structures/dudkin_classwork/WhileTask.java
@@ -0,0 +1,12 @@
+package com.itstep.control.structures.dudkin_classwork;
+
+public class WhileTask {
+    public static void main(String[] args) {
+        int x=0;
+        while(x<=100){
+            System.out.println(x);
+            x++;
+        }
+
+    }
+}
