dd0452b2e4a72e9b594423ad1b94b975498972b8
java <flent25@gmail.com>
2019-10-25 20:43:11 +0300

    SwitchTask

---
+++ control-structures/src/main/java/com/itstep/control/structures/diadiun_classwork/SwitchTask.java
@@ -0,0 +1,28 @@
+package com.itstep.control.structures.diadiun_classwork;
+
+
+
+public class SwitchTask {
+    public static void main(String[] args) {
+        String word="Hello";
+
+    switch (word){
+        case "Hello":
+            System.out.println("Привет");
+            break;
+        case "Bad":
+            System.out.println("Плохой");
+            break;
+        case "Good":
+            System.out.println("Хорошо");
+            break;
+        default:
+            System.out.println("Что то не понятное....");
+    }
+
+
+
+
+    }
+
+}
