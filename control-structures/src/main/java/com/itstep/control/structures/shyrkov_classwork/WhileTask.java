69145408891e8bc5408f4d8964d8dace42f8d7a5
Carnalizer41 <a.shirkov42@gmail.com>
2019-10-25 21:06:16 +0300

    Shyrkov While task

---
+++ control-structures/src/main/java/com/itstep/control/structures/shyrkov_classwork/WhileTask.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.shyrkov_classwork;
+
+public class WhileTask {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 100) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
