b1766ef304da370bff4e1224c92058dd714938c9
Safankov <safankov00@gmail.com>
2019-10-25 21:13:42 +0300

    For task

---
+++ control-structures/src/main/java/com/itstep/control/structures/safankov_classwork/ForTask.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.safankov_classwork;
+
+public class ForTask {
+    public static void main(String[] args) {
+        for(int i = 5;i<=13;i++){
+            if(i==7){
+                System.out.println(i);
+            }
+        }
+    }
+}
