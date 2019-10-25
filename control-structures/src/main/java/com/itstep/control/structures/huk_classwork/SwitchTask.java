d39d50c7063e466d517961d13675cd38b2a957e8
Aleksandr Huk <Gook.goro@gmail.com>
2019-10-25 20:41:54 +0300

    Switch Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/huk_classwork/SwitchTask.java
@@ -0,0 +1,25 @@
+package com.itstep.control.structures.huk_classwork;
+
+public class SwitchTask {
+    public static void main(String[] args) {
+
+        String voice = "Hello";
+
+        switch (voice) {
+            case "Hello":
+                System.out.println("Привет");
+                break;
+            case "Bad":
+                System.out.println("Плохой");
+                break;
+            case "Good":
+                System.out.println("Хороший");
+                break;
+            default:
+                System.out.println("что-то непонятное");
+
+        }
+    }
+}
+
+
