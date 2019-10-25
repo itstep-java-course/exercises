caf69f5fd86cf4e79830aed4770f7cb3bda43e4f
MaximBorovoy <maximborovoy01@gmail.com>
2019-10-25 21:28:47 +0300

    For_Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/borovoy_classwork/ForClass.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.borovoy_classwork;
+
+public class ForClass {
+    public static void main(String[] args) {
+        for (int i = 5; i <=13 ; i++) {
+            if (i==7){
+                System.out.println("The value of i is: " + i);
+            }
+        }
+    }
+}
