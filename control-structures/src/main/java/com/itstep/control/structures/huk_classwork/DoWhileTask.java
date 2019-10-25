38267bbab2099819a57fccdbc3991f29462c39bf
Aleksandr Huk <Gook.goro@gmail.com>
2019-10-25 21:11:41 +0300

    Do While

---
+++ control-structures/src/main/java/com/itstep/control/structures/huk_classwork/DoWhileTask.java
@@ -0,0 +1,12 @@
+package com.itstep.control.structures.huk_classwork;
+
+public class DoWhileTask {
+    public static void main(String[] args) {
+
+        int x = 0;
+        do {
+            System.out.println("The value x is: " + x);
+            x++;
+        } while (x <=100);
+    }
+}
