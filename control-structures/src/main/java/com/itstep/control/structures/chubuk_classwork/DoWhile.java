ec0ab4516e831f02d084de145b122aea2a5033d8
oleh2210 <oleh.chubuk@nure.ua>
2019-10-25 21:09:27 +0300

    Do while and For task

---
+++ control-structures/src/main/java/com/itstep/control/structures/chubuk_classwork/DoWhile.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.chubuk_classwork;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+            x++;
+        } while (x < 101);
+    }
+}
