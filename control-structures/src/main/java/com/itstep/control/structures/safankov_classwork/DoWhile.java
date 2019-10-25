74cd82c8de1e314ba7ac01973d34ce2c1062742f
Safankov <safankov00@gmail.com>
2019-10-25 21:08:58 +0300

    DoWhile task

--- control-structures/src/main/java/com/itstep/control/structures/safankov_classwork/DoWhile.java
+++ control-structures/src/main/java/com/itstep/control/structures/safankov_classwork/DoWhile.java
@@ -1,4 +1,13 @@
 package com.itstep.control.structures.safankov_classwork;
 
 public class DoWhile {
+    public static void main(String[] args) {
+        int x = 0;
+        do{
+            x++;
+            System.out.println(x);
+        }while(x<100);
+
+
+    }
 }
