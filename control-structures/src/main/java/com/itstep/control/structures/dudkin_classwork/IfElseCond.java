ba16f1a46c3c53211c340da7dbe269555af97ffe
VladimirDV2019 <tezzrere@gmail.com>
2019-10-25 20:21:19 +0300

    IF ELSE

---
+++ control-structures/src/main/java/com/itstep/control/structures/dudkin_classwork/IfElseCond.java
@@ -0,0 +1,16 @@
+package com.itstep.control.structures.dudkin_classwork;
+
+public class IfElseCond {
+    public static void main(String[] args) {
+        int x = 20;
+        if (x == 6) {
+            System.out.println("6");
+        } else if (x == 3) {
+            System.out.println("3");
+        } else if (x <= 5) {
+            System.out.println("5");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
