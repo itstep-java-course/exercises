d65d1fc683724119caf25b0d504a2d383a8087c1
MaximBorovoy <maximborovoy01@gmail.com>
2019-10-25 20:22:27 +0300

    If-Else_Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/borovoy_classwork/IfElseTask.java
@@ -0,0 +1,17 @@
+package com.itstep.control.structures.borovoy_classwork;
+
+public class IfElseTask {
+    public static void main(String[] args) {
+        int x = 100;
+        if (x == 6) {
+            System.out.println("x=6");
+        } else if (x == 3) {
+            System.out.println("x=3");
+        } else if (x <= 5) {
+            System.out.println("x<=5");
+        } else {
+            System.out.println("x=0");
+        }
+    }
+}
+
