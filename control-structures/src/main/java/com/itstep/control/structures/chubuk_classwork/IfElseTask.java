248041735c9bc8fb52d39ce9ae099e28b5993933
oleh2210 <oleh.chubuk@nure.ua>
2019-10-25 20:20:33 +0300

    if-else task

---
+++ control-structures/src/main/java/com/itstep/control/structures/chubuk_classwork/IfElseTask.java
@@ -0,0 +1,17 @@
+package com.itstep.control.structures.chubuk_classwork;
+
+public class IfElseTask {
+    public static void main(String[] args) {
+    int x=2;
+        if (x==6) {
+            System.out.println("6");
+        } else if (x==3) {
+            System.out.println("3");
+        } else if (x<=5) {
+            System.out.println("5");
+        } else {
+            System.out.println("0");
+        }
+    }
+
+}
