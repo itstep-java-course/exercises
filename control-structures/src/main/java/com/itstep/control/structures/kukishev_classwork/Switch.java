67e03fdc323b239a4264f7ba6a555a1ae35ac542
GabrielGrehov <gabriel1grehov@gmail.com>
2019-10-25 20:40:00 +0300

    Свичи

---
+++ control-structures/src/main/java/com/itstep/control/structures/kukishev_classwork/Switch.java
@@ -0,0 +1,29 @@
+package com.itstep.control.structures.kukishev_classwork;
+
+public class Switch {
+    public static void main(String[] args) {
+        String word = "Bad";
+
+        switch (word) {
+            case "Hello":
+                System.out.println("Привет");
+                break;
+            case "Good":
+                System.out.println("Хорошо");
+                break;
+            case "Bad":
+                System.out.println("Плохо");
+                break;
+            default:
+                System.out.println("Кравозяблы");
+        }
+
+
+
+    }
+
+
+
+
+
+}
