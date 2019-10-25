76d4af2b0c3478bf5dae0279ef718664c33a969c
java <flent25@gmail.com>
2019-10-25 21:12:18 +0300

    WhileDoTask

---
+++ control-structures/src/main/java/com/itstep/control/structures/diadiun_classwork/WhileDoTask.java
@@ -0,0 +1,15 @@
+package com.itstep.control.structures.diadiun_classwork;
+
+public class WhileDoTask {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println("The value x is: " + x);
+            x++;
+
+
+        } while (x<=100);
+    }
+
+
+}
