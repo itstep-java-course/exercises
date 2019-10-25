3f972afa54d08599b146327f0a5888924fe62d40
MaximBorovoy <maximborovoy01@gmail.com>
2019-10-25 20:42:19 +0300

    Switch_Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/borovoy_classwork/SwitchTask.java
@@ -0,0 +1,23 @@
+package com.itstep.control.structures.borovoy_classwork;
+
+public class SwitchTask {
+    public static void main(String[] args) {
+        String translator = "Hello";
+
+
+        switch (translator) {
+
+            case "Hello":
+                System.out.println("Привет");
+                break;
+            case "Bad":
+                System.out.println("Плохо");
+                break;
+            case "Good":
+                System.out.println("Хорошо");
+                break;
+        }
+    }
+
+
+}
