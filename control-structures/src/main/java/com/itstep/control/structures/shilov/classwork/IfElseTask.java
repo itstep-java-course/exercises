5df296696fabf0969c522a291194d167b2732ca4
javatask3 <1romale1@gmail.com>
2019-10-25 20:21:51 +0300

    task

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/classwork/IfElseTask.java
@@ -0,0 +1,22 @@
+package com.itstep.control.structures.shilov.classwork;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class IfElseTask {
+    public static void main(String[] args) {
+        int x = 6;
+
+        if (x == 6) {
+            System.out.printf("6");
+        } else if (x == 3) {
+            System.out.println();
+        } else if (x >= 5) {
+            System.out.println("3");
+        } else {
+            System.out.println("0");
+        }
+
+
+    }
+}
