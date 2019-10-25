412b83ad3afbb4ee915891b30d97557f6c280b5a
Aleksandr Huk <Gook.goro@gmail.com>
2019-10-25 21:06:15 +0300

    While Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/huk_classwork/WhileTask.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.huk_classwork;
+
+public class WhileTask {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 100) {
+            System.out.println("The x value is: " + x);
+            x++;
+        }
+    }
+}
