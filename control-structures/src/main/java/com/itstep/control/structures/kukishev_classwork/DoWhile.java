dfa3d20f4a37bbdac65cb0652d8140b40690c3bc
GabrielGrehov <gabriel1grehov@gmail.com>
2019-10-25 21:08:46 +0300

    DoWhile

---
+++ control-structures/src/main/java/com/itstep/control/structures/kukishev_classwork/DoWhile.java
@@ -0,0 +1,17 @@
+package com.itstep.control.structures.kukishev_classwork;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int x=0;
+        do {
+            System.out.println("Значение" +x);
+            x++;
+        } while (x>100);
+
+
+
+    }
+
+
+}
