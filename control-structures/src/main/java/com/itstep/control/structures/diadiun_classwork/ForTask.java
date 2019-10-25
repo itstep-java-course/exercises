416e1bd78141d9c49ea0bdfa093fac63f43596b2
java <flent25@gmail.com>
2019-10-25 21:29:32 +0300

    For Task

---
+++ control-structures/src/main/java/com/itstep/control/structures/diadiun_classwork/ForTask.java
@@ -0,0 +1,12 @@
+package com.itstep.control.structures.diadiun_classwork;
+
+public class ForTask {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 13; i++) {
+            if (i == 7) {
+                System.out.println("i");
+            }
+        }
+    }
+
+        }
