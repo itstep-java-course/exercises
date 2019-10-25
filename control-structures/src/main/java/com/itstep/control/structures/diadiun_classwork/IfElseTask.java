9c1dd74f671d79ba455bc8b083a3bda7e3cf0f77
java <flent25@gmail.com>
2019-10-25 20:19:34 +0300

    IfElseTask

---
+++ control-structures/src/main/java/com/itstep/control/structures/diadiun_classwork/IfElseTask.java
@@ -0,0 +1,34 @@
+package com.itstep.control.structures.diadiun_classwork;
+
+public class IfElseTask {
+    public static void main(String[] args) {
+        int x=10;
+        if (x==6) {
+            System.out.println("6");
+        } else if (x==3) {
+            System.out.println("3");
+        } else if (x<=5) {
+            System.out.println("5");
+        }else {
+            System.out.println("0");
+        }
+
+
+
+
+
+
+
+
+
+    }
+    //
+
+
+
+
+
+
+
+    }
+
