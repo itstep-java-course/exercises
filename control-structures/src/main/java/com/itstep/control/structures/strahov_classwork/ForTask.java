a1e088ab12be751f9fb82605a8cf8a8af64616be
Strahov2023 <obormots140@gmail.com>
2019-10-25 21:11:32 +0300

    for task

---
+++ control-structures/src/main/java/com/itstep/control/structures/strahov_classwork/ForTask.java
@@ -0,0 +1,12 @@
+package com.itstep.control.structures.strahov_classwork;
+
+public class ForTask {
+    public static void main(String[] args) {
+        int x = 13;
+        for (int i = 5; i < x; i++) {
+            if (i == 7) {
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
