2ff977f6bc610b7928a3ea977ff7cba5da4e40c5
Safankov <safankov00@gmail.com>
2019-10-25 20:19:47 +0300

    if-else task

---
+++ control-structures/src/main/java/com/itstep/control/structures/safankov_classwork/Ifelse_task.java
@@ -0,0 +1,18 @@
+package com.itstep.control.structures.safankov_classwork;
+
+public class Ifelse_task {
+    public static void main(String[]args){
+      int x = 6;
+      if(x==6){
+          System.out.println("x = 6");
+      }
+       else if(x==3){
+           System.out.println("x = 3");
+        }
+       else if(x<=5){
+           System.out.println("x = 5");
+      }
+       else
+           System.out.println("x = 0");
+    }
+}
