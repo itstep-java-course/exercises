fae04f452fe2e05fd07ca59cb834c5da97fa67fc
MaximBorovoy <maximborovoy01@gmail.com>
2019-10-26 13:02:45 +0300

    Homework

---
+++ control-structures/src/main/java/borovoy_homework/ControlOperatorFor.java
@@ -0,0 +1,10 @@
+package borovoy_homework;
+
+public class ControlOperatorFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.println("The value of i is: " + i);
+        }
+    }
+}
---
+++ control-structures/src/main/java/borovoy_homework/ControlOperatorsWhileBreakContinue.java
@@ -0,0 +1,18 @@
+package borovoy_homework;
+
+public class ControlOperatorsWhileBreakContinue {
+    public static void main(String[] args) {
+        int x = 20;
+        while (x > 0) {
+            if (x % 2 != 0) {
+                x--;
+                continue;
+            } else if (x == 2) {
+                break;
+            }
+            System.out.println("The value of x is: " + x);
+            x--;
+        }
+    }
+}
+
