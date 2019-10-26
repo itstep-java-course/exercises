e76a7a1eabefe778bf5e3a77459daeba3aceefe6
VladimirDV2019 <tezzrere@gmail.com>
2019-10-26 19:51:04 +0300

    Цикл for

---
+++ control-structures/src/main/java/com/itstep/control/structures/dudkin_homework/ForHomeTask.java
@@ -0,0 +1,10 @@
+package com.itstep.control.structures.dudkin_homework;
+
+public class ForHomeTask {
+    public static void main(String[] args) {
+        for (int i = 0; i <=100 ; i++) {
+            if(i%2==0&&i!=0)
+            System.out.println(i);
+        }
+    }
+}
