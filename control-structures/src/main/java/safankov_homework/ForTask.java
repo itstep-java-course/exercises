18b9f579b743bacd289f03a154ab00313cd69449
Safankov <safankov00@gmail.com>
2019-10-27 13:51:49 +0200

    For task

---
+++ control-structures/src/main/java/safankov_homework/ForTask.java
@@ -0,0 +1,11 @@
+package safankov_homework;
+
+public class ForTask {
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
