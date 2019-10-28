04616b7fbc72c7fc63f4d227dd5720726c8f7b9d
Shilov777 <1romale1@gmail.com>
2019-10-28 12:07:31 +0200

    init

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/hometask/ShilovHomeTask1.java
@@ -0,0 +1,35 @@
+package com.itstep.control.structures.shilov.hometask;
+
+import java.sql.SQLOutput;
+
+public class ShilovHomeTask1 {
+     public static void main(String[] args) {
+        int a = 10 ;
+         int b = 2;
+         int c = a + b;
+         System.out.println (c);
+         int d = a - b;
+         System.out.println(d);
+         int e = a * b;
+         System.out.println(e);
+         int f = a / b;
+         System.out.println(f);
+         int g = a % b;
+         System.out.println(g);
+         int h = ++a;
+         System.out.println(h);
+         int i = a +=b;
+         System.out.println(i);
+         int k = a -=b;
+         System.out.println(k);
+         int l = a *=b;
+         System.out.println(l);
+         int m = a /=b;
+         System.out.println(m);
+         int o = a %=b;
+         System.out.println(o);
+         int p = --a;
+         System.out.println(p);
+         int q = a--;
+         System.out.println(q);
+    }}
