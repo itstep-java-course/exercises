8595c66e9cc1b0f22c2d645e1f0ef897910ed097
Shilov777 <1romale1@gmail.com>
2019-10-28 16:21:25 +0200

    init

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/hometask/HomeTask32.java
@@ -0,0 +1,19 @@
+package com.itstep.control.structures.shilov.hometask;
+
+public class HomeTask32 {
+    public class WhileBreakContinueTask {
+        public static void main(String[] args) {
+            int x = 20;
+            while (x >= 0) {
+                if (x % 2 != 0) {
+                    x--;
+                    continue;
+                } else if (x == 2) {
+                    break;
+                }
+                System.out.println("x = " + x);
+                x--;
+            }
+        }
+    }
+}
