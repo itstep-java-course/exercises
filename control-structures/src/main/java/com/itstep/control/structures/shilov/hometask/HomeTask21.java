f6e33fe627a951db67167fde5018759a4c68a836
Shilov777 <1romale1@gmail.com>
2019-10-28 14:21:38 +0200

    init

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/hometask/HomeTask21.java
@@ -0,0 +1,20 @@
+package com.itstep.control.structures.shilov.hometask;
+
+public class HomeTask21 {
+    public class Tern {
+
+        public static void main(String args[]){
+            if (mood == good)
+            {gocourses();
+
+            }
+            else
+            {
+                dontgocourses;
+            }
+
+//When I could do this:
+            int courses = (mood == good) ?gocourses :dontgocourses;
+        }
+    }
+}
