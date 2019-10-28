eb2b280e3d6d0cdfe295440f3ea6462ed65c763f
Shilov777 <1romale1@gmail.com>
2019-10-28 16:17:31 +0200

    init

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/hometask/HomeTask31.java
@@ -0,0 +1,8 @@
+package com.itstep.control.structures.shilov.hometask;
+
+public class HomeTask31 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
