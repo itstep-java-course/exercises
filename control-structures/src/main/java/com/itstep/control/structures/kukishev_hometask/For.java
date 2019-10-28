9786c34429057d6cbe78412a572499576d2d6c97
GabrielGrehov <gabriel1grehov@gmail.com>
2019-10-28 15:21:46 +0200

    For домашка

---
+++ control-structures/src/main/java/com/itstep/control/structures/kukishev_hometask/For.java
@@ -0,0 +1,19 @@
+package com.itstep.control.structures.kukishev_hometask;
+
+public class For {
+
+
+    public static void main(String[] args) {
+
+       for (int i=0; i<=100; i++){
+           if (i%2==0){
+            System.out.println("Значение:" + i);
+        }}
+
+
+    }
+
+
+
+
+}
