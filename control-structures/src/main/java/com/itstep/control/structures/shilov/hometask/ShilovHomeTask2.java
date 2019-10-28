460cbf6acc288f769b0548191e051d5de635d445
Shilov777 <1romale1@gmail.com>
2019-10-28 13:25:37 +0200

    init

---
+++ control-structures/src/main/java/com/itstep/control/structures/shilov/hometask/ShilovHomeTask2.java
@@ -0,0 +1,42 @@
+package com.itstep.control.structures.shilov.hometask;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class ShilovHomeTask2 {
+    public static void main(String[] args) {
+        if (Bosscalled() && standupintime() || notrain()) {
+            public static void main(String[] args){
+              gowork();
+
+        else if (){
+            stayhome();
+        }
+    }
+
+
+
+    private static boolean Bosscalled() {
+        System.out.println("Go work");
+        return true;
+    }
+
+    private static boolean standupintime() {
+        System.out.println("Go work");
+        return true;
+    }
+
+    private static boolean notrain() {
+        System.out.println("Go work");
+        return true;
+    }
+
+    private static void gowork() {
+        System.out.println("Go work");
+    }
+
+    private static void stayhome() {
+        System.out.println("Stay at home.");
+    }
+}
