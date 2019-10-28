3c86536aeba4e0c79567d931b1ed2e5c829381ef
GabrielGrehov <gabriel1grehov@gmail.com>
2019-10-28 13:48:45 +0200

    Switch домашка

---
+++ control-structures/src/main/java/com/itstep/control/structures/kukishev_hometask/Switch.java
@@ -0,0 +1,34 @@
+package com.itstep.control.structures.kukishev_hometask;
+
+
+public class Switch {
+
+    public static void main(String[] args) {
+        String word = "Synchrophasotron";
+
+        switch (word) {
+
+            case "Synchrophasotron":
+                System.out.println("Синхрофазотрон");
+                break;
+
+            case "Space":
+                System.out.println("Космос");
+                break;
+
+            case "Interstellar":
+                System.out.println("Межзвёздный");
+                break;
+
+            default:
+                System.out.println("Загугли");
+
+
+
+
+        }
+    }
+
+
+}
+
