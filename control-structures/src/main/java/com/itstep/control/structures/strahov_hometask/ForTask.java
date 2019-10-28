5ae408c345dc0aa67008cc2faa22261b88806d65
Strahov2023 <obormots140@gmail.com>
2019-10-28 15:15:07 +0200

    hometask 3

---
+++ control-structures/src/main/java/com/itstep/control/structures/strahov_hometask/ForTask.java
@@ -0,0 +1,11 @@
+package com.itstep.control.structures.strahov_hometask;
+
+public class ForTask {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+        if (i%2==0){
+            System.out.println("i="+i);
+        }
+        }
+    }
+}
