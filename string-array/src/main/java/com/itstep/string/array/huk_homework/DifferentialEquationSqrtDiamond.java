a33f84372847ccbb54b5cb5715da71209e3ae8f7
Aleksandr Huk <Gook.goro@gmail.com>
2019-11-02 19:17:08 +0200

    Differential Equation Sqrt Diamond Homework

---
+++ string-array/src/main/java/com/itstep/string/array/huk_homework/DifferentialEquationSqrtDiamond.java
@@ -0,0 +1,15 @@
+package com.itstep.string.array.huk_homework;
+
+/*2. Написать программу которая считает корни дифференциального уравнения.*/
+
+import java.util.Scanner;
+
+public class DifferentialEquationSqrtDiamond {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive integer num: ");
+        int num = sc.nextInt();
+
+    }
+}
