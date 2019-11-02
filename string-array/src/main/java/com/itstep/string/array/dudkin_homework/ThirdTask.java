09d40cc3d0bd3671128f2cd1085e9e71f2d458e1
VladimirDV2019 <tezzrere@gmail.com>
2019-11-02 17:55:54 +0200

    Третье  задание (с разбиением )

--- string-array/src/main/java/com/itstep/string/array/dudkin_homework/ThirdTask.java
+++ string-array/src/main/java/com/itstep/string/array/dudkin_homework/ThirdTask.java
@@ -1,14 +1,13 @@
 package com.itstep.string.array.dudkin_homework;
 
-import javax.swing.*;
 import java.util.Random;
 import java.util.Scanner;
 
-import static java.lang.Math.exp;
 import static java.lang.Math.sqrt;
 
-public class ThirdTask {
+class FirstPart {
     public static void main(String[] args) {
+        System.out.println("Two-dimensional array:");
         int[][] mas = new int[5][];
         for (int i = 0; i < 5; i++)
             mas[i] = new int[2];
@@ -16,13 +15,26 @@ public class ThirdTask {
         for (int i = 0; i < 5; i++)
             for (int j = 0; j < 2; j++) {
                 mas[i][j] = random.nextInt(100) - 50;
-                System.out.println(mas[i][j]);
+                if (j == 0)
+                    System.out.print(mas[i][j] + " ");
+                else
+                    System.out.println(mas[i][j]);
             }
+    }
+}
+
+class SecondPart {
+    public static void main(String[] args) {
         System.out.println("Factorial of 10:");
         int fact = 1;
         for (int i = 10; i != 0; i--)
             fact *= i;
         System.out.println(fact);
+    }
+}
+
+class ThirdPart {
+    public static void main(String[] args) {
         System.out.println("The general solution of the quadratic equation:");
         Scanner is = new Scanner(System.in);
         System.out.println("Enter cons a, b and c");
@@ -34,7 +46,7 @@ public class ThirdTask {
             System.out.println("No valid solution");
         else if (D == 0) {
             double x = (-b / (2 * a));
-            System.out.println("Root of the quadratic equation"+x);
+            System.out.println("Root of the quadratic equation=" + x);
         } else {
             double x1 = ((-b + sqrt(D)) / (2 * a));
             double x2 = ((-b - sqrt(D)) / (2 * a));
