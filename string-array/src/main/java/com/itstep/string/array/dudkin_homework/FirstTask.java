c2349ab29388e8fe577e4bc42e19db49e91c0d23
VladimirDV2019 <tezzrere@gmail.com>
2019-11-02 17:31:26 +0200

    Первое задание(финалочка)

--- string-array/src/main/java/com/itstep/string/array/dudkin_homework/FirstTask.java
+++ string-array/src/main/java/com/itstep/string/array/dudkin_homework/FirstTask.java
@@ -3,27 +3,18 @@ package com.itstep.string.array.dudkin_homework;
 public class FirstTask {
     public static void main(String[] args) {
         String[] array = {"Can", "they", "stay", "for", "tea", "please"};
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < array.length; i++)
             if (array[i].length() <= 5)
                 System.out.println(array[i]);
         System.out.println("");
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < array.length; i++)
             if (array[i].startsWith("p"))
                 System.out.println(array[i]);
         System.out.println("");
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < array.length; i++)
             if (array[i].endsWith("a"))
                 System.out.println(array[i]);
-            char[][]str=new char[6][];
         System.out.println("");
-        int counter=0;
-        for (int i = 0; i <6 ; i++) {
-            str[i] = new char[array[i].length()];
-            str[i]=array[i].toCharArray();
-            counter+=array[i].length();
-            System.out.println(str[i]);
-        }
-        System.out.println("");
-        System.out.println(counter);
+        System.out.println(array.length);
     }
 }
