47908f8defe60db592cd1b7c72ec95d2363f32ba
Carnalizer41 <a.shirkov42@gmail.com>
2019-11-07 19:47:53 +0200

    Shyrkov homework 07.11.2019 (update from lesson)

--- static-method/src/main/java/com/itstep/staticmethod/shyrkov_homework/Task1.java
+++ static-method/src/main/java/com/itstep/staticmethod/shyrkov_homework/Task1.java
@@ -5,6 +5,10 @@ public class Task1 {
         String mainString = "s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz";
         String[] resultArray = extractStrings(mainString);
         printResult(resultArray);
+        String anotherMainString = "s3://part/data/part_path_2/pro/part_path_3/year=2019/month=11/day=04/id=333/file_name.txt";
+        String anotherMoreMainString = "s3://part33/data/part_path_2/processed/part444/year=19/month=11/day=04/id=88/name.gz";
+        printResult(extractStrings(anotherMainString));
+        printResult(extractStrings(anotherMoreMainString));
     }
 
     private static void printResult(String[] resultArray) {
