2e7a1969497336d0c1a60dc550f217a9e9019db9
Carnalizer41 <a.shirkov42@gmail.com>
2019-11-07 18:04:48 +0200

    Shyrkov homework 07.11.2019

---
+++ static-method/src/main/java/com/itstep/staticmethod/shyrkov_homework/Task1.java
@@ -0,0 +1,84 @@
+package com.itstep.staticmethod.shyrkov_homework;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String mainString = "s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz";
+        String[] resultArray = extractStrings(mainString);
+        printResult(resultArray);
+    }
+
+    private static void printResult(String[] resultArray) {
+        for (int i = 0; i < resultArray.length; i++) {
+            if (i != resultArray.length - 1)
+                System.out.print(resultArray[i] + ", ");
+            else
+                System.out.println(resultArray[i]);
+        }
+    }
+
+    private static String[] extractStrings(String mainStr) {
+        return new String[]{getPath(mainStr), getId(mainStr), getDate(mainStr), getName(mainStr)};
+    }
+
+    private static String getPath(String str) {
+        String[] strsArray = str.split("/");
+        return strsArray[2];
+    }
+
+    private static String getId(String str) {
+        String[] strsArray = str.split("/");
+        String res = "";
+        for (String s : strsArray) {
+            if (s.startsWith("id=")) {
+                res = s.substring(3);
+                break;
+            }
+        }
+        return res;
+    }
+
+    private static String getDate(String str) {
+        String[] strsArray = str.split("/");
+        String res = "";
+        res += getYear(strsArray) + "-" + getMonth(strsArray) + "-" + getDay(strsArray);
+        return res;
+    }
+
+    private static String getName(String str) {
+        String[] strsArray = str.split("/");
+        return strsArray[strsArray.length - 1];
+    }
+
+    private static String getYear(String[] strsArray) {
+        String res = "";
+        for (String s : strsArray) {
+            if (s.startsWith("year=")) {
+                res = s.substring(5);
+                break;
+            }
+        }
+        return res;
+    }
+
+    private static String getMonth(String[] strsArray) {
+        String res = "";
+        for (String s : strsArray) {
+            if (s.startsWith("month=")) {
+                res = s.substring(6);
+                break;
+            }
+        }
+        return res;
+    }
+
+    private static String getDay(String[] strsArray) {
+        String res = "";
+        for (String s : strsArray) {
+            if (s.startsWith("day=")) {
+                res = s.substring(4);
+                break;
+            }
+        }
+        return res;
+    }
+}
