d3887302eac1b6d96ec9137e9a7216d7bb4035af
Safankov <safankov00@gmail.com>
2019-11-11 12:56:58 +0200

    All homework)

--- static-method/src/main/java/com/itstep/staticmethod/safankov_homework/StaticMethodTask.java
+++ static-method/src/main/java/com/itstep/staticmethod/safankov_homework/StaticMethodTask.java
@@ -1,39 +1,60 @@
 package com.itstep.staticmethod.safankov_homework;
 
 public class StaticMethodTask {
-    public static String[] convertStringToArray(String text) {
-        String[] array = text.split("/");
-        String[] arr = new String[4];
-        arr[0] = array[2];
-        int id = array[10].indexOf("=")+1;
-        arr[1] = StaticMethodTask.cutIdValue(array[10],id);
-        int year = array[7].indexOf("=")+1;
-        int month = array[8].indexOf("=")+1;
-        int day = array[9].indexOf("=")+1;
-        arr[2] = StaticMethodTask.cutYearMonthDay(array[7],year,array[8],month,array[9],day);
-        int indexFileName = array.length;
-        arr[3] = array[indexFileName-1];
-        return arr;
+    private static String[] cutOnParts(String input) {
+        String partPath = cutPartPath(input);
+        String valueId = cutValueId(input);
+        String date = cutDate(input);
+        String fileName = cutFileName(input);
+        String[] result = new String[]{partPath, valueId, date, fileName};
+        return result;
     }
-    public static String cutIdValue(String idValue, int startIndex)
-    {
-        return idValue.substring(startIndex);
+    private static String cutPartPath(String input) {
+        String[] allInfo = input.split("/");
+        return allInfo[2];
     }
-    public static String cutYearMonthDay(String arrayOfYear,int indexYear,String arrayOfMonth,int indexMonth,String arrayDay,int indexDay)
-    {
-        String date = arrayOfYear.substring(indexYear)+"-"+arrayOfMonth.substring(indexMonth)+"-"+arrayDay.substring(indexDay);
+    private static String cutValueId(String input) {
+        String[] allInfo = input.split("/");
+        String[] idFields = new String[]{};
+        for (int i = 0; i < allInfo.length; i++) {
+            if (allInfo[i].contains("id"))
+                idFields = allInfo[i].split("=");
+        }
+        return idFields[1];
+    }
+    private static String cutDate(String input) {
+        String[] allInfo = input.split("/");
+        String[] yearStr = new String[]{};
+        String[] monthStr = new String[]{};
+        String[] dayStr = new String[]{};
+        for (int i = 0; i < allInfo.length; i++) {
+            String element = allInfo[i];
+            if (element.contains("year"))
+                yearStr = element.split("=");
+            if (element.contains("month"))
+                monthStr = element.split("=");
+            if (element.contains("day"))
+                dayStr = element.split("=");
+        }
+        String date = yearStr[1] + "-" + monthStr[1] + "-" + dayStr[1];
         return date;
     }
-    public static void arrayPrint(String[] arr)
-    {
-        for (String i: arr)
-        {
-            System.out.print(i+", ");
+    private static void printParts(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1)
+                System.out.print(',');
         }
     }
-    public static void main(String[] args)
-    {
-        StaticMethodTask.arrayPrint(convertStringToArray( "s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz"));
+    private static String cutFileName(String input) {
+        int lastIndexOfSlash = input.lastIndexOf("/");
+        return input.substring(lastIndexOfSlash + 1);
     }
+    public static void main(String[] args) {
+        String text = "s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz";
+        String[] parts = cutOnParts(text);
+        printParts(parts);
+    }
+
 }
 
