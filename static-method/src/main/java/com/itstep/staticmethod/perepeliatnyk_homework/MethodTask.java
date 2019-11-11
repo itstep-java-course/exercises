8041116264036aaa8b2221cf7033b3ce796f08d4
Evgeniy <56840720+PerepeliatnykEvgeniy@users.noreply.github.com>
2019-11-11 17:25:59 +0200

    Perepeliatnyk static method (#73)
    * Classwork
    
    * Class object homework

---
+++ static-method/src/main/java/com/itstep/staticmethod/perepeliatnyk_homework/MethodTask.java
@@ -0,0 +1,39 @@
+package com.itstep.staticmethod.perepeliatnyk_homework;
+
+
+//1. написать метод который принимает на вход строку и возвращает массив стрингов:
+//
+//        input string “s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz”
+//
+//        написать логику которая будет выкусывать из строки такие части: part_path_1, значение id, дату и имя файла с расширением
+//        логику, которая выкусывает части строки нужно разместить в отдельных методах
+//        Используйте новые методы внутри главного
+//        главный метод должен возвращать массив String внутри которого будут 4 строки
+//
+//        part_path_1
+//        значение id
+//        дата в формате YYYY-MM-DD
+//        имя файла с расширением
+//
+//
+//        2. написать метод который принимает на вход массив строк (возвращенный первым методом) и выводит их в консоль
+//
+//        Пример вывода:
+//        part_path_1, 11111111, 2019-11-04, file_name.gz
+
+
+import java.sql.SQLOutput;
+
+public class MethodTask {
+    public static void main(String[] args) {
+        String str = "s3://part_path_1/data/part_path_2/processed/part_path_3/year=2019/month=11/day=04/id=11111111/file_name.gz";
+
+
+    }
+
+}
+
+
+
+
+
