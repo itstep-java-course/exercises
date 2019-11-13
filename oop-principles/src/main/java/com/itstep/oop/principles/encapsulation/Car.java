1776f2476a77598428ab38bb0253649af9fadffd
dmitryshnurenko <dimashnurenko@gmail.com>
2019-11-13 22:42:12 +0200

    encapsulation example

--- oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/Car.java
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/Car.java
@@ -3,9 +3,11 @@ package com.itstep.oop.principles.encapsulation;
 public class Car {
 
     private int carSpeed;
+    private int currentTransmissionNumber;
 
     public Car() {
         this.carSpeed = 0;
+        this.currentTransmissionNumber = 0;
     }
 
     public void startEngine() {
@@ -54,7 +56,11 @@ public class Car {
         System.out.println("The clutch has been pushed by car");
     }
 
-    public void setTransmission(int number) {
+    private void setTransmission(int number) {
+        if (number - currentTransmissionNumber > 1) {
+            throw new RuntimeException("заглохла... брббббрбрбрб");
+        }
         System.out.println("The transmission number: " + number + " is being set");
+        currentTransmissionNumber = number;
     }
 }
