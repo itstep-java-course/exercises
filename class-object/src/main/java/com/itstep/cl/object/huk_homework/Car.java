a908ab73663acdddcd11d262ef7c56578c663437
Aleksandr Huk <Gook.goro@gmail.com>
2019-11-14 20:07:22 +0200

    Class-Object-Car-Main-Homework

--- class-object/src/main/java/com/itstep/cl/object/huk_homework/Car.java
+++ class-object/src/main/java/com/itstep/cl/object/huk_homework/Car.java
@@ -1,8 +1,10 @@
 package com.itstep.cl.object.huk_homework;
 
-//import java.util.Date;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+
 
 public class Car {
 
@@ -13,11 +15,12 @@ public class Car {
     private int numberOfSeats; //количество мест
     private double price; //цена
     private double maxSpeed; //максимальная скорость
-    private String date; //дата выпуска
+    private Date date; //дата выпуска
+    //private Date date; //дата выпуска
 
     //конструктор класса
     //тут присходит инициализация при создании Объекта Car
-    public Car(String model, String bodyType, String color, int numberOfSeats, double price, double maxSpeed, String date) { //конструктор класса вызывающейся при создании нового объекта данного класса. Конструкторы выполняют инициализацию объекта
+    public Car(String model, String bodyType, String color, int numberOfSeats, double price, double maxSpeed, Date date) { //конструктор класса вызывающейся при создании нового объекта данного класса. Конструкторы выполняют инициализацию объекта
         //представляет ссылку на текущий экземпляр класса, можем обращаться к переменным, методам объекта, а также вызывать его конструкторы
         this.model = model;
         this.bodyType = bodyType;
@@ -25,11 +28,30 @@ public class Car {
         this.numberOfSeats = numberOfSeats;
         this.price = price;
         this.maxSpeed = maxSpeed;
-        this.date = date;
+        this.date=date;
     }
 
+//    public Car(Date year, Date month, Date day){
+//        this.
+//    }
+
+
+    public static Date StringToDate(String s) {
+
+        Date result = null;
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            result = dateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+
+        }
+        return result;
+    }
+
+
     public String getCarInfo() {
-        return "The car model: " + model + " Body type: " + bodyType + " Color: " + color + " Number of seats: " + numberOfSeats + " Price: " + price + " Max speed: " + " Date: " + date;
+        return "The car model: " + model + " Body type: " + bodyType + " Color: " + color + " Number of seats: " + numberOfSeats + " Price: " + price + " Max speed: " + " Date: "+ date;
     }
 }
 
--- class-object/src/main/java/com/itstep/cl/object/huk_homework/Main.java
+++ class-object/src/main/java/com/itstep/cl/object/huk_homework/Main.java
@@ -1,5 +1,9 @@
 package com.itstep.cl.object.huk_homework;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {//метод Main
         Reverser reverser = new Reverser("hello");
@@ -7,15 +11,24 @@ public class Main {
         System.out.println("=========================================================================================");
 
 
-        //Date date = new Date();
+        Date date1 = Car.StringToDate("2015-12-06 13:13:00");
+        Date date2 = Car.StringToDate("2016-11-06 14:23:00");
+        Date date3 = Car.StringToDate("2017-10-06 15:43:00");
+        //System.out.println(date1);
+        //System.out.println(date2);
+        //System.out.println(date3;
 
-        Car volvo = new Car("S90 Momentum Pro ", "Sedan ", "Metallic", 5, 252_100_000, 204.00, "2016-11-13");
-        Car opel = new Car("Astra Enjoy Plus ", "Sedan ", "Silver ", 5, 442_000_000, 207.00, "2017-12-16");
-        Car bmw = new Car("X7 30d xDrive ", "Сrossover ", "Black ", 5, 212_100_000, 245.00, "2018-10-17");
 
-        System.out.println(volvo.getCarInfo());
-        System.out.println(opel.getCarInfo());
-        System.out.println(bmw.getCarInfo());
+        // System.out.println(date);
+        //System.out.println(date.toString());
+
+        Car volvo = new Car("S90 Momentum Pro ", "Sedan ", "Metallic", 5, 252_100_000, 204.00,date1);
+        Car opel = new Car("Astra Enjoy Plus ", "Sedan ", "Silver ", 5, 442_000_000, 207.00,date2);
+        Car bmw = new Car("X7 30d xDrive ", "Сrossover ", "Black ", 5, 212_100_000, 245.00,date3);
+
+        System.out.println(volvo.getCarInfo().toString());
+        System.out.println(opel.getCarInfo().toString());
+        System.out.println(bmw.getCarInfo().toString());
 
 
 //        String carInfo = volvo.getCarInfo();
