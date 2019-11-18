6d16cfbd940b5338a00b07dcb9a24444b19fc61e
VladimirDV2019 <tezzrere@gmail.com>
2019-11-18 08:55:39 +0200

    Теперь всё

--- oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/dudkin_homework/not_API/OrderManager.java
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/dudkin_homework/not_API/OrderManager.java
@@ -1,9 +1,11 @@
 package com.itstep.oop.principles.encapsulation.dudkin_homework.not_API;
 
 public class OrderManager {
-    Storage firstStorage=new Storage();
+    private Storage firstStorage;
+    public OrderManager(){
+        firstStorage=new Storage();
+    }
     public double purchaseOrder(int orderId, double price) {
-        //Storage firstStorage=new Storage();
         if (!firstStorage.checkAvailability(orderId))
             throw new RuntimeException("We don't have this product!");
         return price + price * 0.2;
