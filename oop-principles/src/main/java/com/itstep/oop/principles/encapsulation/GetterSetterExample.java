0819cbdf68101b60ca8a800af004096a4e20e129
dmitryshnurenko <dimashnurenko@gmail.com>
2019-11-18 20:13:13 +0200

    get set example

--- oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/GetterSetterExample.java
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/GetterSetterExample.java
@@ -1,31 +1,25 @@
 package com.itstep.oop.principles.encapsulation;
 
 public class GetterSetterExample {
-    private String name;
+    public String name;
     private String lastName;
     private int age;
 
-    public String getName() {
-        return name;
+    public GetterSetterExample(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name == null ? "empty" : name;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
--- oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/GetterSetterTest.java
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/GetterSetterTest.java
@@ -4,6 +4,23 @@ public class GetterSetterTest {
 
     //provide an example where need to return default values if field is null
     public static void main(String[] args) {
+        GetterSetterExample test = new GetterSetterExample();
+
+        test.name = "name";
+
+        test.setName("name1");
+
+        System.out.println(test.name == null ? "empty" : test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+        System.out.println(test.name);
+
 
     }
 }
