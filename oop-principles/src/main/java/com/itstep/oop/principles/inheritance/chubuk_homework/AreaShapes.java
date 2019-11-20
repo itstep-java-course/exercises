66b99ed270da06080da7d1873357c019bb502aff
Olezhka <oleh.chubuk@nure.ua>
2019-11-20 21:36:07 +0200

    Homework of shapes

---
+++ oop-principles/src/main/java/com/itstep/oop/principles/inheritance/chubuk_homework/AreaShapes.java
@@ -0,0 +1,19 @@
+package com.itstep.oop.principles.inheritance.chubuk_homework;
+
+public class AreaShapes
+{
+    public static void main(String[] args)
+    {
+        Shape rectangle = new Rectangle("rectangle",14.5, 63.2);
+        Rectangle square = new Square("square", 111, 45.9);
+        Rectangle triangleRe = new RectangularTriangle("rectangular triangle", 3, 5);
+        Shape triangle = new Triangle("triangle", 34, 23.45, 36);
+
+        Shape[] shapes = {rectangle, square, triangle, triangleRe};
+
+        for (Shape shape : shapes)
+        {
+            System.out.println("Area of " + shape.getName() + " is " + shape.getAreaArea());
+        }
+    }
+}
