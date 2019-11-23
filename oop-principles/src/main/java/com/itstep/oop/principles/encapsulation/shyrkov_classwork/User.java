428cf2943e1574e6cd26b24a875c1d192b0a989a
Carnalizer41 <52959051+Carnalizer41@users.noreply.github.com>
2019-11-23 22:59:18 +0200

    Shyrkov oop incapsulation (#81)
    * Shyrkov classwork
    
    * Shyrkov homework
    
    * Shyrkov homework

---
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/shyrkov_classwork/User.java
@@ -0,0 +1,11 @@
+package com.itstep.oop.principles.encapsulation.shyrkov_classwork;
+
+public class User {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+
+        computer.connectToElectricity();
+        computer.turnOn();
+        computer.turnOn();
+    }
+}
