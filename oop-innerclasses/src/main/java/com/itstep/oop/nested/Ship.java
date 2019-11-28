5de0a10d9d40157fe8eeb38fca81deba31529a03
dmitryshnurenko <dimashnurenko@gmail.com>
2019-11-28 21:30:16 +0200

    oop, inner classes

--- oop-innerclasses/src/main/java/com/itstep/oop/nested/Ship.java
+++ oop-innerclasses/src/main/java/com/itstep/oop/nested/Ship.java
@@ -1,12 +1,22 @@
 package com.itstep.oop.nested;
 
 public class Ship {
+    private String model = "AX-12";
+    private static String model2 = "AXX-12";
+
     public static class LifeBoat {
         private int boatId;
+
         public static void down() {
             System.out.println("шлюпки на воду!");
         }
+
         public void swim() {
+
+            Ship ship = new Ship();
+            System.out.println(ship.model);
+
+            System.out.println(model2);
             System.out.println("отплытие шлюпки");
         }
     }
