a1343e5eab8b774ed6dcd545aaaa0d6abb4769b2
Aleksandr Huk <56841069+Gook7Angel@users.noreply.github.com>
2019-11-28 19:42:53 +0200

    oop-principles-encapsulation-orderManager-main-huk  (#95)
    * Oop-Principles-OrderManager-Main-Homework
    
    * Oop-Principles-OrderManager-Main-Homework

--- oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/huk_homework/OrderManager.java
+++ oop-principles/src/main/java/com/itstep/oop/principles/encapsulation/huk_homework/OrderManager.java
@@ -1,56 +1,37 @@
-//package com.itstep.oop.principles.encapsulation.huk_homework;
-///*
-//описать создание заказа на сайте
-//создать класс OrderManager с методом purchaseOrder
-//метод purchaseOrder должен принимать 2 поля: orderId and price
-//метод purchaseOrder должен возвращать стоимость товара расчитанную по формуле, если товар есть на складе, если товара нет бросить эксепшн
-//
-//
-//формула для расчета стоимости товара:
-//price = price + price * 20%
-//
-//Подсказки:
-//
-//- айдишки доступных товаров хранить в классе OrderManager в массиве
-//- пример как бросить эксепшн смотри тут
-//
-// */
-//
-//public class OrderManager { //создали класс OrderManager
-//
-//    private final int[] orderIds;
-//
-//    OrderManager(int[] orderIds) {
-//
-//        this.orderIds = orderIds;
-//    }
-//
-//    public double purchaseOrder(int orderId, double price) { //метод purchaseOrder, метод принимает 2 поля: orderId and price
-//        return price + price * 0.2;
-//
-//    }
-//
-//   double purchaseOrder(int orderId, double price){
-//        if(isOrderExists(orderId)){
-//            daleteExistingOrder(orderId);
-//            return price + price * 0.2;
-//        }
-//   }
-//
-//    private boolean isOrderExists(int orderId) {
-//        for (int id : orderIds) {
-//            if (id == orderId) {
-//                return false;
-//            }
-//
-//        }
-//    }
-//
-//    private void daleteExistingOrder(int orderId){
-//        for (int i = 0; i<orderIds.length; i++){
-//            if(orderIds[i]==orderId){
-//                orderIds[i]=-1;
-//            }
-//        }
-//    }
-//}
+package com.itstep.oop.principles.encapsulation.huk_homework;
+
+public class OrderManager {
+
+
+    private final int[] orderIds;
+
+    public OrderManager(int[] orderIds) {
+        this.orderIds = orderIds;
+    }
+
+    double purchaseOrder(int orderId, double price) {
+        if (isOrderExists(orderId)) {
+            deleteExistingOrder(orderId);
+            return price + price *0.2;
+        }
+        throw new RuntimeException("Order not found");
+    }
+
+    private boolean isOrderExists(int orderId) {
+        for (int id : orderIds) {
+            if (id == orderId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void deleteExistingOrder(int orderId) {
+        for (int i = 0; i < orderIds.length; i++) {
+            if (orderIds[i] == orderId) {
+                orderIds[i] = -1;
+            }
+        }
+    }
+
+}
