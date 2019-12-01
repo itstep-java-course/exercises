fe6a68d9d24b0d00f317dd10f01aca91cbcf726b
Olezhka <oleh.chubuk@nure.ua>
2019-12-01 23:57:41 +0200

    Homework oop all version 3

--- oop-interfaces-innerclasses/src/main/java/com/itstep/oop/interfaces/chubuk_homework/TransformLogicPerform.java
+++ oop-interfaces-innerclasses/src/main/java/com/itstep/oop/interfaces/chubuk_homework/TransformLogicPerform.java
@@ -1,14 +1,21 @@
 package com.itstep.oop.interfaces.chubuk_homework;
 
 public class TransformLogicPerform {
-    private final WordDesigner[] outputLatestVersion;
-    int number = 0;
+    private final WordDesigner[] transformers;
+    private final String[] result;
 
-    public TransformLogicPerform(WordDesigner[] outputLatestVersion) {
-        this.outputLatestVersion = outputLatestVersion;
+    public TransformLogicPerform(WordDesigner[] outputLatestVersion, String[] result) {
+        this.transformers = outputLatestVersion;
+        this.result = result;
     }
 
-    public int performLogic() {
-        return number = TransformLogicPerform.this.outputLatestVersion.length;
+    public String[][] performLogic() {
+        String[][] designer = new String[transformers.length][result.length];
+        String[] tempString;
+        for (int i = 0; i < transformers.length; i++) {
+            tempString = transformers[i].outputLatestVersion(result);
+            designer[i] = tempString;
+        }
+        return designer;
     }
 }
