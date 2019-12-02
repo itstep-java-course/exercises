e152c15cf6c6fea838a86bd0946ea57c1ca03ba2
Olezhka <oleh.chubuk@nure.ua>
2019-12-02 11:29:36 +0200

    Homework oop all version 5

--- oop-interfaces-innerclasses/src/main/java/com/itstep/oop/interfaces/chubuk_homework/Test.java
+++ oop-interfaces-innerclasses/src/main/java/com/itstep/oop/interfaces/chubuk_homework/Test.java
@@ -5,6 +5,13 @@ public class Test {
     public static void main(String[] args) {
         String str = "description,main,maintenance-polimorfism encapsulation, bodies";
         ResultArray resultPrint = new ResultArray();
-        System.out.println(Arrays.deepToString(resultPrint.printAll(str)));
+        String[][] result = resultPrint.printAll(str);
+
+        for (int i = 0; i < result.length; i++){
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
