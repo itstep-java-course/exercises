12e9f970ffc1b2cf35ceeb2119d7667bccf41ca9
dmitryshnurenko <dimashnurenko@gmail.com>
2019-12-13 21:35:30 +0200

    equals and hashcode

--- exceptions/src/main/java/com/itstep/inheritance_exception/Test.java
+++ exceptions/src/main/java/com/itstep/inheritance_exception/Test.java
@@ -1,9 +1,10 @@
 package com.itstep.inheritance_exception;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         FileReaderImpl reader = new FileReaderImpl();
 
        // reader.readFile(new File(""));
--- oop-innerclasses/src/main/java/com/itstep/oop/nested/Test.java
+++ oop-innerclasses/src/main/java/com/itstep/oop/nested/Test.java
@@ -1,5 +1,7 @@
 package com.itstep.oop.nested;
 
+import com.itstep.oop.inner.Car;
+
 public class Test {
     public static void main(String[] args) {
         // вызов статического метода
@@ -8,5 +10,15 @@ public class Test {
         Ship.LifeBoat lifeBoat = new Ship.LifeBoat();
         // вызов обычного метода
         lifeBoat.swim();
+
+
+        Car car = new Car();
+        car.setCarModel("m2");
+        System.out.println(car.hashCode());
+        Car car2 = new Car();
+        car2.setCarModel("m2");
+        System.out.println(car2.hashCode());
+
+        System.out.println(car.equals(car2));
     }
 }
