cff72f1d96528b5ea59054adb0cf04d43514a63a
oleh2210 <56840240+oleh2210@users.noreply.github.com>
2020-01-11 11:07:09 +0200

    I don’t know what the Test is complaining about, but everything else seems to be correct (#128)

---
+++ collections-lists/src/main/java/com/itstep/collections/chubuk_homework/SimpleList.java
@@ -0,0 +1,24 @@
+package com.itstep.collections.chubuk_homework;
+
+public interface SimpleList<T> {
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains(T o);
+
+    boolean add(T o);
+
+    boolean remove(T o);
+
+    void clear();
+
+    T get(int index);
+
+    void add(int index, T element);
+
+    T remove(int index);
+
+    int indexOf(T o);
+}
+
