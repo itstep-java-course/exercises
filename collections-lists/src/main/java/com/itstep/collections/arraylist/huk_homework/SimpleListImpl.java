1d64bb7ebf00fcc4e9598801b8f0ac7d49fa083c
Aleksandr Huk <56841069+Gook7Angel@users.noreply.github.com>
2020-01-11 11:11:43 +0200

    collections lists arraylist Huk (#122)
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk
    
    * simpleList-simplListImppl-test-huk

---
+++ collections-lists/src/main/java/com/itstep/collections/arraylist/huk_homework/SimpleListImpl.java
@@ -0,0 +1,143 @@
+package com.itstep.collections.arraylist.huk_homework;
+
+
+public class SimpleListImpl<T> implements SimpleList<T> {
+
+    private Object[] elements; //поле константа массивов
+
+
+    int index = 0;
+    int count = 0;
+
+    public SimpleListImpl(int initialCapacity) { //конструктор
+        this.elements = new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        for (int i = 0; i < elements.length; i++) {
+            while
+            (index <= count) ;
+            count++;
+           if (index == count)
+               break;
+        }
+            return count;
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return elements.length == 0;
+    }
+
+    @Override
+    public boolean contains(T o) {
+      //return indexOf(o) >= 0;
+        for (int i = 0; i <elements.length ; i++) {
+            if (elements[i]==o);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(T o) { // в конец
+
+        if (index == elements.length) {
+            Object[] temp = new Object[elements.length * 3 / 2 + 1];
+            System.arraycopy(elements,0,temp,0,elements.length);
+            this.elements = temp;
+        }
+        this.elements[index++] = o;// index++ записую обьект
+        return true;
+    }
+
+
+    @Override
+    public void add(int index, T element) { //добавление в середину
+        if (index == elements.length) {
+            Object[] temp = new Object[elements.length * 3 / 2 + 1];
+            System.arraycopy(elements, index, elements, index + 1, elements.length - index);
+            this.elements = temp;
+        }
+        this.elements[index] = element;// index записую обьект
+
+    }
+
+    @Override
+    public boolean remove(T o) {
+//        if (o == null) {
+//            for (int index = 0; index < elements.length; index++)
+//                if (elements[index] == null) {
+//                    fastRemove(index);
+//                    return true;
+//                }
+//        } else {
+//            for (int index = 0; index < elements.length; index++)
+//                if (o.equals(elements[index])) {
+//                    fastRemove(index);
+//                    return true;
+//                }
+//        }
+//        return false;
+        int index = -1;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o)
+                index = i;
+        }
+        if (index == -1)
+            return false;
+        remove(index);
+        return true;
+    }
+
+    @Override
+    public void clear() {
+
+        // clear to let GC do its work
+        for (int index = 0; index < elements.length; index++) {
+            elements[index] = null;
+        }
+//????????????
+    }
+
+
+
+    @Override
+    public T get(int index) {
+        if (index > elements.length - 1)
+            return null;
+        return (T) elements[index]; //нужно только добавить проверку что индекс который ты передаешь в пределах массива
+    }
+
+
+    @Override
+    public T remove(int index) {
+        Object[] objects = elements;
+        int size=0;
+        if (index > elements.length - 1)
+            return null;
+        Object removedElem = elements[index];
+        elements = new Object[elements.length - 1];
+        if (index == 0)
+            System.arraycopy(objects, 1, elements, 0,size);
+        System.arraycopy(objects, 0, elements, 0, index);
+        System.arraycopy(objects,index+1,elements,index+1,elements.length-index-1);
+        return (T) removedElem;
+    }
+
+    @Override
+    public int indexOf(T o) {
+        if (o == null) {
+            for (int i = 0; i < elements.length; i++)
+                if (elements[i]==null)
+                    return i;
+        } else {
+            for (int i = 0; i < elements.length; i++)
+                if (o.equals(elements[i]))
+                    return i;
+        }
+       return -1;
+   }
+}
