d75755f1f5184652011c474becfc0c18ae10620c
Aleksandr Huk <Gook.goro@gmail.com>
2020-01-14 13:22:00 +0200

    Добавил ЕTable c его реализацией

---
+++ collections-lists/src/main/java/com/itstep/collections/huk_homework/table/RectangleTable.java
@@ -0,0 +1,13 @@
+package com.itstep.collections.huk_homework.table;
+
+public class RectangleTable extends Table{
+
+    public RectangleTable(String name, double surfaceArea) {
+        super(name, surfaceArea);
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
