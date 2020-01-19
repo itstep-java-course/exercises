7a5dd83b6394e9a37c7a7d6dfc82df8684099fe5
Dmitry Shnurenko <dimashnurenko@gmail.com>
2020-01-19 14:03:22 +0200

    Revert "homework"

--- collections-lists/src/main/java/com/itstep/collections/arraylist/RectangleTable.java
+++
@@ -1,7 +0,0 @@
-package com.itstep.collections.arraylist;
-
-public class RectangleTable extends Table {
-    RectangleTable(double surfaceSquare, String name) {
-        super(surfaceSquare, name);
-    }
-}
