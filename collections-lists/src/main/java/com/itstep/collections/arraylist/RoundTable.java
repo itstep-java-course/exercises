8ce82341e62639041bed116fb12c8d4f272a1809
Dmitry Shnurenko <dimashnurenko@gmail.com>
2020-01-19 14:03:47 +0200

    Revert "homework" (#137)

--- collections-lists/src/main/java/com/itstep/collections/arraylist/RoundTable.java
+++
@@ -1,7 +0,0 @@
-package com.itstep.collections.arraylist;
-
-public class RoundTable extends Table {
-    RoundTable(double surfaceSquare, String name) {
-        super(surfaceSquare, name);
-    }
-}
--- collections-lists/src/main/java/com/itstep/collections/arraylist/SquareTable.java
+++
@@ -1,8 +0,0 @@
-package com.itstep.collections.arraylist;
-
-public class SquareTable extends RectangleTable {
-    SquareTable(double surfaceSquare, String name) {
-        super(surfaceSquare, name);
-    }
-
-}
