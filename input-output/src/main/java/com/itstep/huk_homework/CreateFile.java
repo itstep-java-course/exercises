19ac47a6b5f00083d03cb1de737dd620ef5514f1
Aleksandr Huk <Gook.goro@gmail.com>
2020-01-29 21:08:33 +0200

    Created Directory and Files

--- input-output/src/main/java/com/itstep/huk_homework/CreateFile.java
+++ input-output/src/main/java/com/itstep/huk_homework/CreateFile.java
@@ -1,7 +1,10 @@
 package com.itstep.huk_homework;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 /*
 ht_javaIO
@@ -48,7 +51,67 @@ id;name;last_name;street;house;age
 
         public class CreateFile {
 
+
+            public  void whenWriteStringUsingBufferedWriter1(File file1) {
+                String str = "1;Petr;Maksimov\n" +
+                        "5;Ivan;Egorov\n" +
+                        "2;Petr;Rakul\n" +
+                        "3;Miha;Dobkin\n" +
+                        "4;Danil;Maksimov";
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("input-output/src/main/java/com/itstep/huk_homework/myData/text1.txt"))) {
+                    writer.write(str);
+                    writer.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            public  void whenWriteStringUsingBufferedWriter2(File file2) {
+                String str = "1;Gagarina;25\n" +
+                        "2;Pushkina;17\n" +
+                        "4;Nauki;7\n" +
+                        "5;Bajana;33\n" +
+                        "3;Derjavinska;18";
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("input-output/src/main/java/com/itstep/huk_homework/myData/text2.txt"))) {
+                    writer.write(str);
+                    writer.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+//            public  void whenWriteStringUsingBufferedWriter3(File file1, File file2) {
+//                String str = "";
+//                try (BufferedWriter writer = new BufferedWriter(new FileWriter("input-output/src/main/java/com/itstep/huk_homework/myData/text3.txt"))) {
+//                    writer.write(str);
+//                    writer.newLine();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+
+            private static void readFileLineByLineOldSchool() throws IOException {
+                final File file = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text3.txt");
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    List<String> lines = new ArrayList<>();
+                    while ((line = reader.readLine()) != null) {
+                        lines.add(line);
+                    }
+
+                    System.out.println(lines);
+                }
+            }
+
+
+
+
+
             public static void main(String[] args) throws IOException {
+
+
 
                 final File dir = new File("input-output/src/main/java/com/itstep/huk_homework/myData");
                 dir.mkdirs();
@@ -56,8 +119,7 @@ id;name;last_name;street;house;age
 
                 if (dir.exists()) {
                     System.out.println("Folder exists");
-                }
-                else if (! dir.exists()){
+                } else if (!dir.exists()) {
                     System.out.println("There is no such folder, I create a new one");
                     final File dir1 = new File("input-output/src/main/java/com/itstep/huk_homework/myData");
                     dir1.mkdirs();
@@ -66,16 +128,17 @@ id;name;last_name;street;house;age
 
                 final File file1 = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text1.txt");
                 final File file2 = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text2.txt");
+                final File file5 = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text3.txt");
                 file1.createNewFile();
                 file2.createNewFile();
+                file5.createNewFile();
 //              file1.delete();
 //              file2.delete();
 
 
                 if (file1.exists() && file2.exists()) {
                     System.out.println("Files created");
-                }
-                else if(! (file1.exists() && file2.exists())){
+                } else if (!(file1.exists() && file2.exists())) {
                     System.out.println("There are no such files, I create new ones");
                     final File file3 = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text1.txt");
                     final File file4 = new File("input-output/src/main/java/com/itstep/huk_homework/myData/text2.txt");
@@ -84,6 +147,15 @@ id;name;last_name;street;house;age
                 }
 
 
-            }
-        }
 
+
+                CreateFile createFile = new CreateFile();
+                createFile.whenWriteStringUsingBufferedWriter1(file1);
+                createFile.whenWriteStringUsingBufferedWriter2(file2);
+
+               CreateFile.readFileLineByLineOldSchool();
+            }
+
+
+
+        }
