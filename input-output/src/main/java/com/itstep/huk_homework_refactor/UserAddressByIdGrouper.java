80d52d35fecaa64174f951e1e5fe01fd5368d9b7
Aleksandr Huk <Gook.goro@gmail.com>
2020-01-30 21:31:45 +0200

    Created

--- input-output/src/main/java/com/itstep/huk_homework_refactor/UserAddressByIdGrouper.java
+++ input-output/src/main/java/com/itstep/huk_homework_refactor/UserAddressByIdGrouper.java
@@ -3,7 +3,7 @@ package com.itstep.huk_homework_refactor;
 import java.util.List;
 
 public class UserAddressByIdGrouper {
-    public String group (List<Address> list, List<User> list2) {
-return null;
+    public String group(List<Address> list, List<User> list2) {
+        return null;
     }
 }
