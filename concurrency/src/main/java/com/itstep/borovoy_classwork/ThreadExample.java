80421a340de7411131cfad75b9121c58a6ed88af
Maxim Borovoy <maximborovoy01@gmail.com>
2020-02-03 21:29:37 +0200

    borovoy_classwork

---
+++ concurrency/src/main/java/com/itstep/borovoy_classwork/ThreadExample.java
@@ -0,0 +1,16 @@
+package com.itstep.borovoy_classwork;
+
+public class ThreadExample {
+    public static void main(String[] args) {
+
+        final Thread thread = new Thread(() -> {
+            System.out.println("Thread:" + Thread.currentThread().getName());
+            for (int i = 1; i < 10; i++) {
+                System.out.println(i);
+            }
+        });
+        thread.start();
+
+    }
+}
+
