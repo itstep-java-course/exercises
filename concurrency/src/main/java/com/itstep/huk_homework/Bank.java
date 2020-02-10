ade36bdeea67ff220539fe3c29378456f29d0a60
Aleksandr Huk <Gook.goro@gmail.com>
2020-02-10 19:02:58 +0200

    В процессе переписывания с книги

--- concurrency/src/main/java/com/itstep/huk_homework/Bank.java
+++ concurrency/src/main/java/com/itstep/huk_homework/Bank.java
@@ -25,5 +25,16 @@ public Bank(int n, double initialBalance){
  * @param amount Сумма перевода
  */
 
+public void transfer (int from, int to, double amount){
+    if (accounts[from]< amount)  return;
+    System.out.println(Thread.currentThread());
+    accounts[from] -=amount;
+    System.out.printf("%10.2f с 2f %d на %d",  amount, from, to);
+    accounts[to] +=amount;
+    System.out.printf("Общий балансЖ %10.2f%n", getTotalBalance());
+}
+/**
+ *
+ */
 
 }
